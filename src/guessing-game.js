1b2352d9b9685bda2c22bc3076dc86de4f5ee760
Forlocks <volgaopa@gmail.com>
2023-10-23 00:07:18 +0300

    init: start guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min + 1;
+        this.max = max - 1;
     }
 
     guess() {
-
+        this.mid = Math.round(((this.min + this.max) / 2));
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid - 1;
     }
 
     greater() {
-
+        this.min = this.mid + 1;
     }
 }
 
